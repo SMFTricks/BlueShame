0c7cec14d3a732b90e0f050161636555b3fd31b1
Diego Andrés <diegoandres_cortes@outlook.com>
2020-01-27 13:43:49 -0600

    Initial Release
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ languages/Settings.english.php
@@ -0,0 +1,9 @@
+<?php
+// Version: 2.0; Settings
+
+global $settings;
+
+$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
+$txt['theme_description'] = 'Blue Shame.<br /><br />Author: <a href="https://smftricks.com">SMF Tricks Team</a>';
+
+?>
---
+++ languages/ThemeStrings.english.php
@@ -0,0 +1,12 @@
+<?php
+
+$txt['sh_info'] = 'Information';
+$txt['st_menu'] = 'Menu';
+
+$txt['facebook_username'] = 'Facebook username';
+$txt['twitter_username'] = 'Twitter username';
+$txt['youtube_username'] = 'Youtube id';
+$txt['rss_url'] = 'RSS URL';
+$txt['sh_social_desc'] = 'Write the username or the id to show the button link in the top bar.';
+
+?>
